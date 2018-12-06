6fe6541ed8a67088c156dd0c7ddd501ec1335c34
qiuyuli <qiuyuli@Ctrip.com>
2018-12-06 23:51:01 +0800

    add structure

--- react-mobx/src/App.js
+++ react-mobx/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
 
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        hello world
+        todoList的lenth是{this.props.todoList.length}
       </div>
     );
   }
